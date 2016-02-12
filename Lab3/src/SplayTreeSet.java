09c190c4f23a0ad16252614f60f72c203ab05459
krkn0001 <kristoffer_knutsson@hotmail.com>
2016-02-12 16:18:49 +0100

    Stackoverflowlulz
    Some edge case causes a circular reference between nodes

--- Lab3/src/SplayTreeSet.java
+++ Lab3/src/SplayTreeSet.java
@@ -1,6 +1,7 @@
 public class SplayTreeSet<E  extends Comparable<? super E>> implements SimpleSet<E> {
 
     private Node root;
+    private int size = 0;
 
     public SplayTreeSet() {
         this.root = null;
@@ -30,43 +31,89 @@ public class SplayTreeSet<E  extends Comparable<? super E>> implements SimpleSet
 
     @Override
     public int size() {
-        return 0;
+        return size;
     }
 
     @Override
     public boolean add(E x) {
-        Node node = add(root, null, x);
+        Node node = addRec(root, null, x);
 
         if (node == null) {
             return false;
+        }else if(root == null) {
+            root = node;
         }
-
         doSplay(node);
-
+        size++;
         return true;
     }
 
     private void doSplay(Node node) {
-        if (node.data.compareTo(root.data) == 0) {
 
-        } else if (node.parent.data.compareTo(root.data) == 0) {
-            // TODO: Zig
-        } else if (node.isLeftChild() == node.parent.isLeftChild()){
-            // TODO: ZigZig
-        } else if(node.isLeftChild() != node.parent.isLeftChild()){
-            // TODO: ZigZag
+        while(node.data.compareTo(root.data) != 0) {
+            if (node.parent.data.compareTo(root.data) == 0) {
+                //is node a right or left child? if right rotateleft - if left rotateright
+                if(node.isLeftChild()){
+                    rotateRight(node);
+                }else{
+                    rotateLeft(node);
+                }
+            } else if (node.isLeftChild() == node.parent.isLeftChild()) { // If node and it's parents both are both left children or right children
+                // if node and parent are leftchildren, rotate parent and gparent right, then node and gparent right
+                if(node.isLeftChild()){
+                    rotateRight(node.parent);
+                    rotateRight(node);
+                }else{
+                    rotateLeft(node.parent);
+                    rotateLeft(node);
+                }
+            } else if (node.isLeftChild() != node.parent.isLeftChild()) { //If node and it's parent are not both left children or right children
+                if(node.isLeftChild()){
+                    rotateRight(node);
+                    rotateLeft(node);
+                }else{
+                    rotateLeft(node);
+                    rotateRight(node);
+                }
+            }
         }
     }
 
-    private Node add(Node current, Node parent, E x) {
+    private void rotateLeft(Node node){
+        Node parent = node.parent;
+
+        parent.right = node.left;
+        node.left = parent;
+        node.parent = parent.parent;
+        parent.parent = node;
+        updateRoot(node, parent);
+    }
+
+    private void rotateRight(Node node){
+        Node parent = node.parent;
+
+        parent.left = node.right;
+        node.right = parent;
+        node.parent = parent.parent;
+        parent.parent = node;
+        updateRoot(node, parent);
+    }
+
+    private void updateRoot(Node node, Node parent){
+        if(parent.data.compareTo(root.data) == 0){
+            root = node;
+        }
+    }
+
+    private Node addRec(Node current, Node parent, E x) {
         if (current == null) {
             current = new Node(x);
             current.parent = parent;
             return current;
         } else if (x.compareTo(current.data) < 0) {
-            return add(current.left, current, x);
+            return addRec(current.left, current, x);
         } else if (x.compareTo(current.data) > 0) {
-            return add(current.right, current, x);
+            return addRec(current.right, current, x);
         }
 
         return null;
@@ -74,11 +121,70 @@ public class SplayTreeSet<E  extends Comparable<? super E>> implements SimpleSet
 
     @Override
     public boolean remove(E x) {
-        return false;
+        Node node = containsRec(root, x);
+        if(node == null){
+            return false;
+        }
+        if(root.data.compareTo(node.data) == 0){
+            root = null;
+        }else if(node.left == null && node.right == null){
+            if(node.isLeftChild()){
+                node.parent.left = null;
+            }else{
+                node.parent.right = null;
+            }
+        }else if(node.left == null){
+            if(node.isLeftChild()){
+                node.parent.left = node.right;
+            }else{
+                node.parent.right = node.right;
+            }
+        }else if(node.right == null){
+            if(node.isLeftChild()){
+                node.parent.left = node.left;
+            }else{
+                node.parent.right = node.left;
+            }
+        }else if(node.left != null && node.right != null){
+            Node lrNode = findRightMostNode(node.left);
+            node.data = lrNode.data;
+            lrNode.parent.right = lrNode.left;
+        }else{
+            return false;
+        }
+        size--;
+        return true;
+    }
+
+    private Node findRightMostNode(Node node){ //Find the (in this case) right most node under parameter Node
+        if(node.right == null){
+            return node;
+        }
+        return findRightMostNode(node.right);
     }
 
     @Override
     public boolean contains(E x) {
+        Node node = containsRec(root, x);
+        if(node != null){
+            doSplay(node);
+            return true;
+        }
         return false;
     }
+
+    public Node containsRec(Node node, E x){
+        if(node == null) {
+            return null;
+        }else if(node.data.compareTo(x) > 0) {
+            return containsRec(node.left, x);
+        }else if(node.data.compareTo(x) < 0){
+            return containsRec(node.right, x);
+        }
+        return node;
+    }
+
+
+
+
 }
---
+++ Lab3/src/TestSetCorrectness.java
@@ -0,0 +1,70 @@
+import java.util.Random;
+import java.util.Set;
+import java.util.TreeSet;
+
+/**
+ * Created by Kristoffer on 2016-02-12.
+ */
+public class TestSetCorrectness {
+
+    private static SimpleSet<Integer> set;
+    private static Set<Integer> javaSet;
+    private static int restarts, operationsPerTest, interval, n1, n2, n3, n4;
+    private static int removeFail, removeSuccess, addFail, addSuccess, containsFail, containSuccess, sizeFail, sizeSuccess;
+
+    public static void main(String[] args){
+
+        n1 = Integer.parseInt(args[0]);
+        n2 = Integer.parseInt(args[1]);
+        n3 = Integer.parseInt(args[2]);
+        n4 = Integer.parseInt(args[3]);
+
+
+        interval = n4;
+        Random random = new Random();
+
+        for(restarts = n2 ; restarts > 0 ; restarts--){
+            if(n1 == 1){
+                set = new SortedLinkedListSet<Integer>();
+            }else if(n1 == 2){
+                set = new SplayTreeSet<Integer>();
+            }
+            javaSet = new TreeSet<Integer>();
+
+            for(operationsPerTest = n3 ; operationsPerTest > 0 ; operationsPerTest--){
+
+                int methodChooser = random.nextInt(4);
+                int randomInt = random.nextInt(interval);
+                if(methodChooser == 0){
+                    if(set.add(randomInt) == javaSet.add(randomInt)){
+                        addSuccess++;
+                    }else addFail++;
+                }else if(methodChooser == 1){
+                    if(set.contains(randomInt) == javaSet.contains(randomInt)){
+                        containSuccess++;
+                    }else containsFail++;
+                }else if(methodChooser == 2){
+                    if(set.remove(randomInt) == javaSet.remove(randomInt)){
+                        removeSuccess++;
+                    }else removeFail++;
+                }else{
+                    if(set.size() == javaSet.size()){
+                        sizeSuccess++;
+                    }else sizeFail++;
+                }
+
+
+                //set.add(random.nextInt(interval-1));
+            }
+        }
+        System.out.println("Adds successfull: " + addSuccess);
+        System.out.println("Adds failure: " + addFail);
+        System.out.println("Removes successfull: " + removeSuccess);
+        System.out.println("Remove fails: " + removeFail);
+        System.out.println("Contains successfull: " + containSuccess);
+        System.out.println("Contain fails: " + containsFail);
+        System.out.println("Size successfull: " + sizeSuccess);
+        System.out.println("Size fails: " + sizeFail);
+
+    }
+}
