ebba6b65c85d518a3c9194c795d83e779764dec3
krkn0001 <kristoffer_knutsson@hotmail.com>
2016-03-01 18:24:10 +0100

    Kruskals working

--- Lab4/src/CompKruskalEdge.java
+++ Lab4/src/CompKruskalEdge.java
@@ -1,3 +1,19 @@
+import java.util.Comparator;
 
-public class CompKruskalEdge {
+public class CompKruskalEdge<E extends Edge> implements Comparator<E> {
+
+
+    @Override
+    public int compare(E o1, E o2) {
+        if(o1.getWeight() -  o2.getWeight() > 0){
+            return 1;
+        }else if(o1.getWeight() -  o2.getWeight() < 0){
+            return -1;
+        }else return 0;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        return true;
+    }
 }
--- Lab4/src/DirectedGraph.java
+++ Lab4/src/DirectedGraph.java
@@ -2,14 +2,23 @@
 import java.util.*;
 
 public class DirectedGraph<E extends Edge> {
-
+	private List<E>[] nodes;
+	private PriorityQueue<E> pq;
+	private Vector<LinkedList<E>> nodeVector;
 
 	public DirectedGraph(int noOfNodes) {
-		;
+		System.out.println("Number of nodes: " + noOfNodes);
+		nodes = (List<E>[]) new List[noOfNodes];
+		for(int i = 0 ; i< noOfNodes ; i++){ 					//O(n)
+			nodes[i] = new LinkedList<E>();						//O(1)
+		}
 	}
 
 	public void addEdge(E e) {
-		;
+		System.out.println("Source is: " + e.getSource());
+		nodes[e.getSource()].add(e);							//O(1)
+		//pq.add(e);
+
 	}
 
 	public Iterator<E> shortestPath(int from, int to) {
@@ -17,7 +26,50 @@ public class DirectedGraph<E extends Edge> {
 	}
 		
 	public Iterator<E> minimumSpanningTree() {
-		return null;
+		LinkedList<E> shorterList, longerList;
+		pq = new PriorityQueue<E>(new CompKruskalEdge());
+		nodeVector = new Vector<>(); //Vector with list of edges, this will be used for a return;
+
+		for(int i = 0 ; i < nodes.length ; i++){ //Off by one?
+			nodeVector.add(new LinkedList<E>());
+		}
+
+		for(List<E> list : nodes){
+			for(E e : list){
+				pq.add(e);
+			}
+		}
+
+		mainloop:
+		while(!pq.isEmpty()){
+			E e;
+			do{
+				e = pq.poll();
+				if(e == null){
+					break mainloop;
+				}
+
+			}while(nodeVector.get(e.getSource()) == nodeVector.get(e.getDest()));
+
+			longerList = nodeVector.get(e.getDest());
+			shorterList = nodeVector.get(e.getSource());
+			if(nodeVector.get(e.getSource()).size() > nodeVector.get(e.getDest()).size()){
+				longerList = nodeVector.get(e.getSource());
+				shorterList = nodeVector.get(e.getDest());
+			}
+
+			longerList.add(e);
+			nodeVector.set(e.getDest(), longerList);
+			nodeVector.set(e.getSource(), longerList);
+			longerList.addAll(shorterList);
+			for(E edges : shorterList){
+				nodeVector.set(edges.getSource(), longerList);
+				nodeVector.set(edges.getDest(), longerList);
+			}
+
+		}
+
+		return nodeVector.get(1).iterator();
 	}
 
 }
