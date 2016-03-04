3b2fe73fbd22d2bbe1cc2e190ff9706d702d66cd
Wassburgare <daniel.wassbjer@gmail.com>
2016-03-04 15:11:26 +0100

    Optimize the compare

--- Lab4/src/CompDijkstraPath.java
+++ Lab4/src/CompDijkstraPath.java
@@ -4,16 +4,6 @@ public class CompDijkstraPath<E extends Edge> implements Comparator<Path<E>> {
 
     @Override
     public int compare(Path<E> path1, Path<E> path2) {
-        Double pathLength1 = 0.0;
-        for (E edge : path1.getPath()) {
-            pathLength1 += edge.getWeight();
-        }
-
-        Double pathLength2 = 0.0;
-        for (E edge : path2.getPath()) {
-            pathLength2 += edge.getWeight();
-        }
-
-        return pathLength1.compareTo(pathLength2);
+        return ((Double)path1.getDistance()).compareTo(path2.getDistance());
     }
 }
